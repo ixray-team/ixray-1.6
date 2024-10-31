b9e4fddc5d3993c2ce5714e0ab74763fc2cf0dd5
Hozar2002 <hoz2002team@gmail.com>
2024-10-31 13:29:50 +0300

    Implement 3D PDA
    Co-authored-by: ForserX <sv3nk@yandex.ru>

--- src/xrEngine/EngineExternal.h
+++ src/xrEngine/EngineExternal.h
@@ -24,7 +24,8 @@ enum class EEngineExternalGame
 	EnableWeaponInertion,
 	EnableWeaponCollision,
 	EnableArtefactDegradation,
-	DisableSprintWhileOverweight
+	DisableSprintWhileOverweight,
+	Enable3DPDA
 };
 
 enum class EEngineExternalRender {
